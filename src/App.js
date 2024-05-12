da927806214c23af730e74b461632ceba7feb76f
masyitaandinii <masyitaandini@gmail.com>
2024-05-12 15:08:31 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Home from "./Home";
+import Home from "./User/Home";
+import Navbar from "./User/Navbar"
+import Menu from "./User/Menu";
+import Footer from "./User/Footer";
+import SignIn from "./User/SignIn";
+import Signup from "./User/SignUp";
+import MenuDetail from "./User/MenuDetail"
 
 function App() {
   return (
-  <Home/>
+  <MenuDetail/>
   );
 }
 
---
+++ src/User/MenuDetail.jsx
@@ -0,0 +1,191 @@
+function MenuDetail() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-[1440px] h-[1772.42px] px-[100px] py-20 bg-white flex-col justify-center items-center gap-14 inline-flex">
+        <div className="self-stretch h-[328.63px] bg-blue-800 rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+          <div className="self-stretch p-10 justify-start items-end gap-[18.26px] inline-flex">
+            <div className="text-white text-[64px] font-medium font-['Manrope'] leading-[76.80px]">
+              Kantin Bu Yun
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[1227.79px] flex-col justify-start items-center gap-8 flex">
+          <div className="self-stretch justify-center items-start gap-8 inline-flex">
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Nasi Pecel
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  15.000
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Mojito
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  8.000
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Nasi Ayam Katsu
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  12.000
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch justify-center items-start gap-8 inline-flex">
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Nasi Pecel
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  15.000
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] bg-black/opacity-50 rounded-xl flex-col justify-center items-center gap-3 flex">
+                <div className="opacity-20 w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+                <div className="px-6 py-3 rounded-[40px] border border-white justify-center items-center gap-2.5 inline-flex">
+                  <div className="text-center text-white text-2xl font-medium font-['Manrope'] leading-9">
+                    Add To Cart
+                  </div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Mojito
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  8.000
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Nasi Ayam Katsu
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  12.000
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch justify-center items-start gap-8 inline-flex">
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Nasi Pecel
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  15.000
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Mojito
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  8.000
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 h-[387.93px] flex-col justify-start items-start gap-2 inline-flex">
+              <div className="self-stretch h-[328.63px] rounded-xl flex-col justify-end items-center gap-[18.26px] flex">
+                <div className="w-[86.84px] h-[87.82px] relative">
+                  <div className="w-[44.91px] h-[13.30px] left-[20.96px] top-[8.28px] absolute"></div>
+                  <div className="w-[45.06px] h-[12.79px] left-[20.91px] top-[66.90px] absolute"></div>
+                  <div className="w-[63.39px] h-[17.98px] left-[12.24px] top-[27.27px] absolute"></div>
+                  <div className="w-[62.57px] h-[11.82px] left-[12.60px] top-[49.21px] absolute"></div>
+                </div>
+              </div>
+              <div className="self-stretch px-3 py-2 justify-between items-center inline-flex">
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  Nasi Ayam Katsu
+                </div>
+                <div className="text-neutral-700 text-2xl font-medium font-['Manrope'] leading-9">
+                  12.000
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default MenuDetail;
